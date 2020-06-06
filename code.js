c07d71ad55b0c19a79431e2c0821eb6b04738557
Hayder Ameen <hayder204@gmail.com>
2020-06-06 23:11:46 +0500

    Update code.js
    More responsive

--- code.js
+++ code.js
@@ -1,22 +1,49 @@
-let sticky = document.getElementById("sticky");
-      let limit = document.getElementById("limiting-element-div");
-      sticky.style.height = "91vh";
+document.onreadystatechange = () => {
+  if (document.readyState === "complete") {
+    if (screen.width > 759) {
+      let carousel = document.getElementById("stickyHalf");
+      let limit = document.getElementById("set-height-resize");
+      carousel.style.height = "91vh";
+      if (screen.width / screen.height < 1) {
+        // It is a tablet
+        if (screen.orientation.angle === 0) {
+          carousel.style.height = "44vh";
+        } else {
+          carousel.style.height = "79vh";
+        }
+        document.getElementById("slideshow-holder").style.height = "auto";
+        document.getElementById("slideshow-holder").style.width =
+          "calc(40vw - 6px)";
+      }
       window.onscroll = function () {
         let topOffset = limit.getBoundingClientRect().bottom;
         let windowHeight = window.innerHeight;
         if (topOffset > windowHeight + 80) {
-          sticky.style.position = "fixed";
-          sticky.style.bottom = "0";
-          sticky.style.right = "0";
-          sticky.style.top = "";
+          carousel.style.position = "fixed";
+          carousel.style.bottom = "0";
+          carousel.style.right = "0";
+          carousel.style.top = "";
         } else {
-          let top = limit.clientHeight;
-          sticky.style.position = "absolute";
-          sticky.style.top = top - sticky.clientHeight + "px";
-          sticky.style.right = "0";
+          let top = document.getElementById("set-height-resize").clientHeight;
+          carousel.style.position = "absolute";
+          carousel.style.top = top - carousel.clientHeight + "px";
+          carousel.style.right = "0";
         }
       };
+      // When tablets change orientation
+      window.onorientationchange = function () {
+        if (screen.orientation.angle === 0) {
+          carousel.style.height = "44vh";
+        } else {
+          carousel.style.height = "79vh";
+        }
+      };
+    }
+  }
+};
 
 // some improvements and basically change to entire method
 
 // Now, it detects based on exact height of a reference div or parent div!
+
+// Added for tablets, and it tracks the orientation of tablets too.
