5b9c851dc961018b1520e4611b125c25c9507ed7
Hayder Ameen <hayder204@gmail.com>
2020-06-06 21:26:46 +0500

    Better logic
    Much improved and dynamic

--- code.js
+++ code.js
@@ -1,22 +1,22 @@
-  let sticky = this.document.getElementById("sticky-element");
-  let limit = this.document.getElementById(
-    "offset/limit element"
-  );
-  window.onscroll = function () {
-    let topOffset = limit.getBoundingClientRect().top;
-    let windowHeight = window.innerHeight;
-    if (topOffset > windowHeight + 100) {
-      sticky.style.position = "fixed";
-      sticky.style.bottom = "0";
-      sticky.style.right = "0";
-    } else {
-      sticky.style.position = "absolute";
-      sticky.style.bottom =
-        sticky.offsetHeight +
-        windowHeight -
-        limit.offsetHeight * 2 +
-        100 +
-        "px";
-      sticky.style.right = "0";
-    }
-  };
+let sticky = document.getElementById("sticky");
+      let limit = document.getElementById("limiting-element-div");
+      sticky.style.height = "91vh";
+      window.onscroll = function () {
+        let topOffset = limit.getBoundingClientRect().bottom;
+        let windowHeight = window.innerHeight;
+        if (topOffset > windowHeight + 80) {
+          sticky.style.position = "fixed";
+          sticky.style.bottom = "0";
+          sticky.style.right = "0";
+          sticky.style.top = "";
+        } else {
+          let top = limit.clientHeight;
+          sticky.style.position = "absolute";
+          sticky.style.top = top - sticky.clientHeight + "px";
+          sticky.style.right = "0";
+        }
+      };
+
+// some improvements and basically change to entire method
+
+// Now, it detects based on exact height of a reference div or parent div!
